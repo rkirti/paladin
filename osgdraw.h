af4834734f81be0c6272f2deab6614c6617e64f0
Kirtika Ruchandani <kirtibr@gmail.com>
2010-11-15 05:04:12 +0530

    Some changes to the file structure

---
+++ osgdraw.h
@@ -0,0 +1,59 @@
+#ifndef  OSGDRAW_H
+#define  OSGDRAW_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <osg/LightSource>
+#include <osg/LightModel>
+#include <osg/CullFace>
+#include <osgCal/CoreModel>
+#include <osgCal/Model>
+#include <osg/CameraView>
+#include <osg/Quat>
+#include <osgViewer/Viewer>
+#include <osg/PositionAttitudeTransform>
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+
+#include <osg/NodeCallback>
+
+#include <osgGA/MatrixManipulator>
+#include <osgGA/KeySwitchMatrixManipulator>
+#include <osgText/Text>
+#include <osgGA/NodeTrackerManipulator>
+
+#include "movement.h"
+#include "physics.h"
+
+
+osg::ref_ptr<osg::Geode> createWall();
+osg::ref_ptr<osgCal::Model> createModel(const std::string fileName);
+void followTheModel(osgViewer::Viewer* viewer, osgCal::Model *model);
+
+
+class AnimationToggleHandler : public osgGA::GUIEventHandler 
+{
+    public: 
+
+        AnimationToggleHandler( osgCal::Model* m, palladinPosition *palPosPtr)
+            : model( m )
+            , currentAnimation( -1 )
+        {
+            palPos = palPosPtr;
+        }
+        
+        bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa);
+        
+    
+    private:
+
+        osgCal::Model*              model;
+        // std::vector< std::string >  animationNames;
+        int                         currentAnimation;
+        palladinPosition *palPos;
+};
+
+#endif 
